dff590b3a874cf57a02d501bdb9588936ffc73be
Muhammad-Khairul-Kholqi <khairulkhlqi31@gmail.com>
2025-10-03 19:07:30 +0700

    update hero section in home page

--- src/components/sections/home/HeroSection.vue
+++ src/components/sections/home/HeroSection.vue
@@ -1,15 +1,9 @@
 <template>
     <div class="flex justify-center p-5">
         <div class="w-full max-w-[1300px]">
-            <div class="flex justify-center mt-5">
-                <div v-motion="motionAnimation.createDelayedAnimation(motionAnimation.slideRight, 100)" class="border border-gray-200 rounded-full px-3 py-2 flex items-center gap-2">
-                    <Users class="w-4 h-4 text-blue-500" />
-                    <span>100K+ users worldwide</span>
-                </div>
-            </div>
-
             <div class="text-center space-y-2 mt-5">
-                <h1 v-motion="motionAnimation.bottomToTop" class="text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-bold">
+                <h1 v-motion="motionAnimation.bottomToTop"
+                    class="text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-bold">
                     Make Learning<br> Fun With <span class="text-blue-600">Quizzes</span>
                 </h1>
                 <div v-motion="motionAnimation.bottomToTopDelayed" class="flex justify-center">
@@ -20,18 +14,33 @@
                 </div>
             </div>
 
-            <button v-motion="motionAnimation.createDelayedAnimation(motionAnimation.slideUp, 1000)" class="flex justify-center m-auto">
-                <RouterLink to=""
-                    class="group mt-5 flex justify-center items-center gap-2 border border-gray-200 px-3 py-2 rounded-full">
+            <div class="mt-10 flex flex-wrap gap-5 justify-center items-center">
+                <RouterLink v-motion="motionAnimation.createDelayedAnimation(motionAnimation.slideLeft, 1000)" to="" class="group flex justify-center items-center gap-2 border border-gray-200 px-3 py-2 rounded-full">
                     <span>Get Started</span>
-                    <div class="bg-blue-600 p-2 rounded-full relative overflow-hidden">
+                    <div class="bg-blue-600 p-1 rounded-full relative overflow-hidden">
                         <ArrowUpRight
                             class="w-5 h-5 text-white relative z-10 transition-all duration-500 group-hover:translate-x-0 group-hover:translate-y-0 translate-x-[-150%] translate-y-[150%]" />
                         <ArrowUpRight
                             class="w-5 h-5 text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-500 group-hover:translate-x-[150%] group-hover:translate-y-[-150%]" />
                     </div>
                 </RouterLink>
-            </button>
+
+                <RouterLink v-motion="motionAnimation.createDelayedAnimation(motionAnimation.slideRight, 1000)" to=""
+                    class="border border-gray-200 px-3 py-2.5 rounded-full hover:bg-gray-50">
+                    <span>See Today's Article</span>
+                </RouterLink>
+            </div>
+
+            <div class="mt-10 flex justify-center">
+                <div class="w-full max-w-[800px]">
+                    <div class="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1 items-center gap-10">
+                        <div v-for="(data, index) in staticsData" :key="index" v-motion="motionAnimation.createDelayedAnimation(motionAnimation.bottomToTop, 1200 + index * 200)" class="text-center space-y-1">
+                            <h2 class="text-4xl font-bold">{{ data.number }}</h2>
+                            <p class="text-gray-500 text-sm">{{ data.label }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -40,4 +49,10 @@
 import { RouterLink } from 'vue-router';
 import { ArrowUpRight, Users } from 'lucide-vue-next';
 import * as motionAnimation from "../../animation/motionAnimation"
+
+const staticsData = [
+    { number: '100K+', label: 'USERS WORLDWIDE' },
+    { number: '200', label: 'FUN QUIZZES' },
+    { number: '150', label: 'CATEGORIES QUIZZES' },
+]
 </script>
