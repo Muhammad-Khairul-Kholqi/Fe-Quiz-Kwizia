b8613b9a11558f95d6c501bd1a59f913399d9ff7
Muhammad-Khairul-Kholqi <khairulkhlqi31@gmail.com>
2025-10-10 20:04:02 +0700

    fix bug production

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import Prerender from 'vite-plugin-prerender'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
-    Prerender({
-      staticDir: 'dist',
-      routes: ['/', '/blog', '/leaderboard', '/faq', '/contact-us'],
-    })
   ],
 })
